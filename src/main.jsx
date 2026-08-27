c36863d0727230acb416f2951541d83da82121eb
Ingrid Burger <igb34@drexel.edu>
2026-08-27 11:17:35 -0400

    case mismatch

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './context/theme-context';
-import App from './app';
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
